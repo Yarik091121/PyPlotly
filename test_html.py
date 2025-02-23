db5d2c784a36069b7740f2e0bfe1a31f9d153e19
Yarik091121 <sergeevyarik7@gmail.com>
2025-02-24 00:15:29 +0300

    add 3rd theme

--- test_html.py
+++ test_html.py
@@ -1,47 +1,163 @@
-# Import packages
-from dash import Dash, html, dash_table, dcc, callback, Output, Input
-import pandas as pd
+# # Import packages
+# from dash import Dash, html, dash_table, dcc, callback, Output, Input
+# import pandas as pd
+# import plotly.express as px
+
+# # Incorporate data
+# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+
+# # Initialize the app - incorporate css
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+# app = Dash(external_stylesheets=external_stylesheets)
+
+# # App layout
+# app.layout = [
+#     html.Div(className='row', children='My First App with Data, Graph, and Controls',
+#              style={'textAlign': 'center', 'color': 'blue', 'fontSize': 30}),
+
+#     html.Div(className='row', children=[
+#         dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'],
+#                        value='lifeExp',
+#                        inline=True,
+#                        id='my-radio-buttons-final',
+#                        style={'background': '#81D4FA'})
+#     ]),
+
+#     html.Div(className='row', children=[
+#         html.Div(className='six columns', children=[
+#             dash_table.DataTable(data=df.to_dict('records'), page_size=11, style_table={'overflowX': 'auto'})
+#         ]),
+#         html.Div(className='six columns', children=[
+#             dcc.Graph(figure={}, id='histo-chart-final')
+#         ])
+#     ])
+# ]
+
+# # Add controls to build the interaction
+# @callback(
+#     Output(component_id='histo-chart-final', component_property='figure'),
+#     Input(component_id='my-radio-buttons-final', component_property='value')
+# )
+# def update_graph(col_chosen):
+#     fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+#     return fig
+
+# # Run the app
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+from dash import Dash, dcc, html, Input, Output, callback
+
 import plotly.express as px
 
-# Incorporate data
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+import json
+import pandas as pd
 
-# Initialize the app - incorporate css
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = Dash(external_stylesheets=external_stylesheets)
 
-# App layout
-app.layout = [
-    html.Div(className='row', children='My First App with Data, Graph, and Controls',
-             style={'textAlign': 'center', 'color': 'blue', 'fontSize': 30}),
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+styles = {
+    'pre': {
+        'border': 'thin lightgrey solid',
+        'overflowX': 'scroll'
+    }
+}
+
+df = pd.DataFrame({
+    "x": [1,2,1,2],
+    "y": [1,2,3,4],
+    "customdata": [1,2,3,4],
+    "fruit": ["apple", "apple", "orange", "orange"]
+})
+
+fig = px.scatter(df, x="x", y="y", color="fruit", custom_data=["customdata"])
+
+fig.update_layout(clickmode='event+select')
+
+fig.update_traces(marker_size=20)
+
+app.layout = html.Div([
+    dcc.Graph(
+        id='basic-interactions',
+        figure=fig
+    ),
 
     html.Div(className='row', children=[
-        dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'],
-                       value='lifeExp',
-                       inline=True,
-                       id='my-radio-buttons-final',
-                       style={'background': '#81D4FA'})
-    ]),
+        html.Div([
+            dcc.Markdown("""
+                **Hover Data**
 
-    html.Div(className='row', children=[
-        html.Div(className='six columns', children=[
-            dash_table.DataTable(data=df.to_dict('records'), page_size=11, style_table={'overflowX': 'auto'})
-        ]),
-        html.Div(className='six columns', children=[
-            dcc.Graph(figure={}, id='histo-chart-final')
-        ])
+                Mouse over values in the graph.
+            """),
+            html.Pre(id='hover-data', style=styles['pre'])
+        ], className='three columns'),
+
+        html.Div([
+            dcc.Markdown("""
+                **Click Data**
+
+                Click on points in the graph.
+            """),
+            html.Pre(id='click-data', style=styles['pre']),
+        ], className='three columns'),
+
+        html.Div([
+            dcc.Markdown("""
+                **Selection Data**
+
+                Choose the lasso or rectangle tool in the graph's menu
+                bar and then select points in the graph.
+
+                Note that if `layout.clickmode = 'event+select'`, selection data also
+                accumulates (or un-accumulates) selected data if you hold down the shift
+                button while clicking.
+            """),
+            html.Pre(id='selected-data', style=styles['pre']),
+        ], className='three columns'),
+
+        html.Div([
+            dcc.Markdown("""
+                **Zoom and Relayout Data**
+
+                Click and drag on the graph to zoom or click on the zoom
+                buttons in the graph's menu bar.
+                Clicking on legend items will also fire
+                this event.
+            """),
+            html.Pre(id='relayout-data', style=styles['pre']),
+        ], className='three columns')
     ])
-]
+], style={'background': 'white', 'border-radius': '3px'})
 
-# Add controls to build the interaction
+
 @callback(
-    Output(component_id='histo-chart-final', component_property='figure'),
-    Input(component_id='my-radio-buttons-final', component_property='value')
-)
-def update_graph(col_chosen):
-    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
-    return fig
+    Output('hover-data', 'children'),
+    Input('basic-interactions', 'hoverData'))
+def display_hover_data(hoverData):
+    return json.dumps(hoverData, indent=2)
 
-# Run the app
+
+@callback(
+    Output('click-data', 'children'),
+    Input('basic-interactions', 'clickData'))
+def display_click_data(clickData):
+    return json.dumps(clickData, indent=2)
+
+
+@callback(
+    Output('selected-data', 'children'),
+    Input('basic-interactions', 'selectedData'))
+def display_selected_data(selectedData):
+    return json.dumps(selectedData, indent=2)
+
+
+@callback(
+    Output('relayout-data', 'children'),
+    Input('basic-interactions', 'relayoutData'))
+def display_relayout_data(relayoutData):
+    return json.dumps(relayoutData, indent=2)
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
